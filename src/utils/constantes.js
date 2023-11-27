996d77cf2f9ee2e7b71dbd9b245d06df0a45114f
dmytro-liashok <lyashokdima1@gmail.com>
2023-11-27 17:06:03 +0200

    setting routes

---
+++ src/utils/constantes.js
@@ -0,0 +1,44 @@
+//Routes
+
+export const WELCOME_ROUTE = "/welcome";
+export const SIGNUP = "/signup";
+export const SIGNIN = "/signin";
+export const PROFILE_ROUTE = "/profile";
+export const DIARY_ROUTE = "/diary";
+export const PRODUCTS_ROUTE = "/products";
+export const EXERCISES_ROUTE = "/exercises";
+
+//Backend request
+
+//Backend request user
+
+export const SIGNUP_REQUEST = "/users/register";
+export const VERIFY_REQUEST = "/users/verify";
+export const SIGNIN_REQUEST = "/users/login";
+export const LOGOUT_REQUEST = "/users/logout";
+export const CURRENT_REQUEST = "/users/current";
+
+//Backend request setting
+
+export const AVATAR_REQUEST = "/users/avatar";
+export const SETTINGS_REQUEST = "/profileSettings";
+
+//Backend request exercises
+
+export const EXERCISES_REQUEST = "/exercises";
+export const EXERCISES_TYPES_REQUEST = "/exercises/types";
+
+//Backend request products
+
+export const PRODUCTS_REQUEST = "/products";
+export const PRODUCTS_CATEGORY_REQUEST = "/products/category";
+
+//Backend request diary
+
+export const DIARY_REQUEST = "/diary";
+export const DIARY_PRODUCTS_REQUEST = "/diary/products";
+export const DIARY_EXERCISES_REQUEST = "/diary/exercises";
+
+//Backend request statistics
+
+export const STATISTICS_REQUEST = "/statistics";
